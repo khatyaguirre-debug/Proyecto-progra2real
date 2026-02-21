f4b6469631590609ccb681aac2d4ecec490ac63d
khatyaguirre-debug <khatyaguirre@gmail.com>
2026-02-21 19:53:03 +0000

    cambios

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ import pandas as pd
 st.title ("Dashboard customer shopping")
 
 # Cargar datos
-df = pd.read_csv("custumer_shopping_data.csv")
+df = pd.read_csv("customer_shopping_data.csv")
 
 st.write("Vista previa del dataset:")
 st.dataframe(df.head())
+
+# Total ventas por shopping mall
+ventas_mall = df.groupby("shopping_mall")["price"].sum()
+
+st.subheader("Ventas por shopping mall")
+st.bar_chart(ventas_mall)
